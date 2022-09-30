577ea1a2c5084b7a1d8a3a4f6151398d4e9da8f3
Emilio Rivers <rivers.emilio2001@protonmail.com>
2022-09-30 12:53:57 -0500

    Created component

---
+++ src/components/QuestionTestCard.tsx
@@ -0,0 +1,12 @@
+import './QuestionTestCard.css';
+
+interface ContainerProps {
+  name: string;
+}
+
+const QuestionTestCard: React.FC<ContainerProps> = ({ name }) => {
+  return (
+  );
+};
+
+export default QuestionTestCard;
