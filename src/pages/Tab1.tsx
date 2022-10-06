12818989dd2b8385f4aadfb2e7382acc848bf0e6
pedrofdzl <pedroandresfdz@hotmail.com>
2022-10-05 22:37:45 -0500

    Added Tutorial card

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,12 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import TutorialCard from '../components/TutorialCard';
 import './Tab1.css';
+
+const exampleCard = {
+  message: 'Bienvenidos a Sostek, este es un tutorial.',
+  imageUrl: '/assets/gota.png'
+}
 
 const Tab1: React.FC = () => {
   return (
@@ -11,6 +17,7 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <TutorialCard message={exampleCard.message} imageUrl={exampleCard.imageUrl}></TutorialCard>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
