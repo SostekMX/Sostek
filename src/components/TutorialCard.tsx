7dfa1154af4dce32e93b9906db217b76abe44380
pedrofdzl <pedroandresfdz@hotmail.com>
2022-10-10 16:22:18 -0500

    Card slides, dialogue box and more

--- src/components/TutorialCard.tsx
+++ src/components/TutorialCard.tsx
@@ -1,28 +1,52 @@
 import { IonIcon, IonButton, IonButtons } from "@ionic/react";
-import { close } from "ionicons/icons";
+import { Icon } from "ionicons/dist/types/components/icon/icon";
+import { arrowForward, close } from "ionicons/icons";
 import { useRef, useState } from "react";
 import './TutorialCard.css';
 
 interface CardProps {
-    message: string;
-    character: string; // Ej. gota1, gota2, mundo1, maceta3
-    align: string; // Opciones: left, right
+  message: string;
+  character: string; // Ej. gota1, gota2, mundo1, maceta3
+  align: string; // Opciones: left, right
 }
 
-const TutorialCard: React.FC<CardProps> = (props) => {
+interface CardSlides {
+  slides: CardProps[];
+}
+
+const TutorialCard: React.FC<CardSlides> = (props) => {
+  const nSlides = props.slides.length;
+  const [currSlide, nextSlide] = useState(0);
+  const onClickNext = () => nextSlide(currSlide+1);
   const [className, setClassName] = useState('tutorial-card');
-  const onClick = () => setClassName('tutorial-card-hide');
-  const characterImgUrl = '/assets/characters/'+props.character+'.png';
+  const onClickEnd = () => setClassName('tutorial-card-hide');
+  var currIcon;
+  var currAction;
+  var dialogueClass = "dialogue-box db-"+props.slides[currSlide].align;
+  if(currSlide == nSlides-1){
+    currIcon = close;
+    currAction = onClickEnd;
+  }else{
+    currIcon = arrowForward;
+    currAction = onClickNext;
+  }
+  var characterImgUrl:string[] = [];
+  for(var i = 0; i < nSlides; i++){
+    const aux = '/assets/characters/'+props.slides[i].character+'.png'
+    characterImgUrl.push(aux);
+  }
 
   return (
     <div className={className}>
       <div className="block-background"></div>
       <IonButtons className="tutorial-card-button">
-        <IonButton type="submit" onClick={onClick}><IonIcon icon={close}></IonIcon></IonButton>
+        <IonButton type="submit" onClick={currAction}><IonIcon icon={currIcon}></IonIcon></IonButton>
       </IonButtons>
       <div className="tutorial-card-body">
-        <p>{props.message}</p>
-        <img src={characterImgUrl} className={props.align}></img>
+        <div className={dialogueClass}>
+          <p>{props.slides[currSlide].message}</p>
+        </div>
+        <img src={characterImgUrl[currSlide]} className={props.slides[currSlide].align}></img>
       </div>
     </div>
   );
