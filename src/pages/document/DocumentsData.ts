80e578bc73066e7808a5f446a94b40f7b3ebfaa8
GrePaC <A01366730@tec.mx>
2022-10-12 01:13:50 -0500

    Vertical carrousel

--- src/pages/document/DocumentsData.ts
+++ src/pages/document/DocumentsData.ts
@@ -4,20 +4,23 @@ export const dummyArticles = [
         "title" : "Desarrollo Sostenible", 
         "description": "Breve deescripción para mostrar", 
         "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar accumsan ligula id venenatis. Ut a arcu est. Nulla venenatis quam et dolor gravida efficitur. Quisque ac sapien turpis. Praesent tristique ut dui id vulputate. Pellentesque rutrum vel nisi vel cursus. Etiam in placerat mi. Nunc mattis gravida nisl vitae luctus. Aenean in nibh posuere, vulputate lectus ac, efficitur lectus. Duis porttitor sollicitudin convallis. Ut condimentum vehicula dolor at condimentum. Cras iaculis, justo varius dignissim gravida, nisi augue egestas urna, id rhoncus magna urna a orci. Ut eu dui leo. Morbi nisl ipsum, finibus quis dolor ut, viverra venenatis lectus. Quisque at nunc sit amet neque tempor ullamcorper id at urna.", 
-        "url_img": "https://cevad.net/wp-content/uploads/2021/02/Desarrollo_sostenible.jpg"
+        "url_img": "https://cevad.net/wp-content/uploads/2021/02/Desarrollo_sostenible.jpg",
+        "type": "articulo"
     },
     {
         "id": "234", 
         "title" : "Desarrollo Sostenible 2", 
         "description": "Breve deescripción para mostrar", 
         "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar accumsan ligula id venenatis. Ut a arcu est. Nulla venenatis quam et dolor gravida efficitur. Quisque ac sapien turpis. Praesent tristique ut dui id vulputate. Pellentesque rutrum vel nisi vel cursus. Etiam in placerat mi. Nunc mattis gravida nisl vitae luctus. Aenean in nibh posuere, vulputate lectus ac, efficitur lectus. Duis porttitor sollicitudin convallis. Ut condimentum vehicula dolor at condimentum. Cras iaculis, justo varius dignissim gravida, nisi augue egestas urna, id rhoncus magna urna a orci. Ut eu dui leo. Morbi nisl ipsum, finibus quis dolor ut, viverra venenatis lectus. Quisque at nunc sit amet neque tempor ullamcorper id at urna.", 
-        "url_img": "https://s1.significados.com/foto/9dd4-des-sus_bg.jpg"
+        "url_img": "https://s1.significados.com/foto/9dd4-des-sus_bg.jpg",
+        "type": "presentacion"
     },
     {
         "id": "567", 
         "title" : "Desarrollo Sostenible 3", 
         "description": "Breve deescripción para mostrar", 
         "content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc pulvinar accumsan ligula id venenatis. Ut a arcu est. Nulla venenatis quam et dolor gravida efficitur. Quisque ac sapien turpis. Praesent tristique ut dui id vulputate. Pellentesque rutrum vel nisi vel cursus. Etiam in placerat mi. Nunc mattis gravida nisl vitae luctus. Aenean in nibh posuere, vulputate lectus ac, efficitur lectus. Duis porttitor sollicitudin convallis. Ut condimentum vehicula dolor at condimentum. Cras iaculis, justo varius dignissim gravida, nisi augue egestas urna, id rhoncus magna urna a orci. Ut eu dui leo. Morbi nisl ipsum, finibus quis dolor ut, viverra venenatis lectus. Quisque at nunc sit amet neque tempor ullamcorper id at urna.", 
-        "url_img": "https://static9.depositphotos.com/1641036/1122/v/450/depositphotos_11227376-stock-illustration-green-earth-sustainable-development-concept.jpg"
+        "url_img": "https://static9.depositphotos.com/1641036/1122/v/450/depositphotos_11227376-stock-illustration-green-earth-sustainable-development-concept.jpg",
+        "type": "articulo"
     },
 ]
