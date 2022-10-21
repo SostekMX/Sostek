ea1a7ae863924d41cb265f3f29d1ff78d4d4e9a4
Joaquin Beltran <contact@joaquinmx.com>
2022-10-21 05:07:33 -0500

    fix: fix model that disappear with rebase

--- src/pages/tab1/Tab1.tsx
+++ src/pages/tab1/Tab1.tsx
@@ -9,28 +9,37 @@ import { dummyArticles } from '../document/DocumentsData';
 import ArticleCarrousel from '../../components/ArticleCarrousel';
 import { useEffect, useState } from 'react';
 import ArticleCardModalWrapper from '../../components/ArticleCardModalWrapper';
-
+import { File } from '../../models/File';
 
 const exampleCard = {
-  message: 'Bienvenidos a Sostek, este es un tutorial. Probamos mas texto para ver la alineacion del contenido.',
+  message: 'Bienvenidos a Sostek, este es un tutorial.',
   character: 'gota2',
   align: 'right'
 }
+const exampleCard1 = {
+  message: 'En esta pestaña puedes informarte con artículos y presentaciones.',
+  character: 'gota2',
+  align: 'left'
+}
 const exampleCard2 = {
-  message: 'Bienvenidos a Sostek, este es otro tutorial.',
+  message: 'Si solo te interesa un tipo, puedes filtrarlos con el botón arriba de mí.',
   character: 'mundo1',
+  align: 'right'
+}
+
+const exampleCard3 = {
+  message: 'También debo recordarte que esta pestaña se actualiza quincenalmente.',
+  character: 'mundo1',
+  align: 'right'
+}
+
+const exampleCard4 = {
+  message: 'Esto es todo por el momento, mientras avances en la app te seguiremos guiando.',
+  character: 'gota2',
   align: 'left'
 }
 const tutorialSlides = {
-  slides: [exampleCard, exampleCard2]
-}
-
-const dummyArticle = {
-  imageUrl : "/assets/article-img.jpeg",
-  title : "Nombre del artículo",
-  subtitle : "Subtitulo del articulo",
-  author : "Fulanito de Tal",
-  body : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+  slides: [exampleCard, exampleCard1, exampleCard2, exampleCard3, exampleCard4]
 }
 
 const Tab1: React.FC = () => {
@@ -79,6 +88,7 @@ const Tab1: React.FC = () => {
         
         {!hasNotBeenCalled && <> 
           <ArticleCardModalWrapper fileId={lastFile?.id} />
+          <TutorialCard slides={tutorialSlides.slides} />
           <ArticleCarrousel files={files} />
         </>
         }
