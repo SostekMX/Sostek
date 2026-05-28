56b32044c8379bb1d101851b6e852f8c3b3ff370
LeNav23 <A01286259@tec.mx>
2026-05-28 10:20:03 -0600

    fix: reconectar campos del perfil al estado y al backend

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -4,65 +4,65 @@ import axios from 'axios';
 
 import './Profile.css'
 
-
-
 const Profile: React.FC = () => {
 
-    const [email, setEmail] = useState< string | null>('');
-    const [name, setName] = useState< string | null>('');
-    const [surname, setSurname] = useState< string | null>('');
+    const [email, setEmail] = useState<string>('');
+    const [name, setName] = useState<string>('');
+    const [surname, setSurname] = useState<string>('');
+    const [birthDate, setBirthDate] = useState<string>('');
+    const [occupation, setOccupation] = useState<string>('');
+    const [gender, setGender] = useState<string>('');
     const [message, setMessage] = useState<string>('');
     const [showAlert, setShowAlert] = useState<boolean>(false);
 
     useEffect(() => {
-        //NativeStorage.getItem('user_email').then(
-        // data => setEmail(data)
-        //);
-        let user_email = sessionStorage.getItem('user_email');
-        setEmail(user_email);
-    }, []) 
+        setEmail(localStorage.getItem('user_email') ?? '');
+    }, []);
 
     function editUser() {
         axios.post('http://localhost:8080/user/edit', {
-            email: email,
-            name: name,
-            surname: surname
+            email,
+            name,
+            surname,
+            birth_date: birthDate,
+            occupation,
+            gender,
         }).then(function (response) {
-            setMessage(response.data.message)
-            setShowAlert(true)
+            setMessage(response.data.message);
+            setShowAlert(true);
         }).catch(function (error) {
             console.log(error);
         });
     }
-    
+
     return (
-      <IonContent fullscreen class='bg-img'>
-        <IonRow class='modify-form'>
+        <IonContent fullscreen class='bg-img'>
+            <IonRow class='modify-form'>
                 <h2>Modificar Perfil</h2>
             </IonRow>
             <IonRow class='space'></IonRow>
             <IonRow className='align-center'>
                 <IonItem color='none' className='input-field'>
-                    <IonLabel position='stacked' >Nombre</IonLabel> 
-                    <IonInput  value={name} onIonChange={(e) => setName(e.target.value as string)} type='text'></IonInput>
+                    <IonLabel position='stacked'>Nombre</IonLabel>
+                    <IonInput value={name} onIonChange={(e) => setName(e.target.value as string)} type='text' />
                 </IonItem>
                 <IonItem color='none' className='input-field'>
-                    <IonLabel position='stacked' >Apellido</IonLabel> 
-                    <IonInput  value={surname} onIonChange={(e) => setSurname(e.target.value as string)} type='text' placeholder='[Apellido actual]'></IonInput>
+                    <IonLabel position='stacked'>Apellido</IonLabel>
+                    <IonInput value={surname} onIonChange={(e) => setSurname(e.target.value as string)} type='text' />
                 </IonItem>
                 <IonItem color='none' className='input-field'>
-                    <IonLabel position='stacked' >Año de nacimiento</IonLabel> 
-                    <IonInput type='date'></IonInput>
+                    <IonLabel position='stacked'>Fecha de nacimiento</IonLabel>
+                    <IonInput value={birthDate} onIonChange={(e) => setBirthDate(e.target.value as string)} type='date' />
                 </IonItem>
                 <IonItem color='none' className='input-field'>
-                    <IonLabel position='stacked' >Ocupación</IonLabel> 
-                    <IonInput type='text' placeholder='[Ocupación actual]'></IonInput>
+                    <IonLabel position='stacked'>Ocupación</IonLabel>
+                    <IonInput value={occupation} onIonChange={(e) => setOccupation(e.target.value as string)} type='text' />
                 </IonItem>
                 <IonItem color='none' className='input-field'>
-                    <IonLabel position='stacked' >Sexo</IonLabel> 
-                    <IonSelect>
-                      <IonSelectOption value="masculino">Masculino</IonSelectOption>
-                      <IonSelectOption value="femenino">Femenino</IonSelectOption>
+                    <IonLabel position='stacked'>Sexo</IonLabel>
+                    <IonSelect value={gender} onIonChange={(e) => setGender(e.target.value as string)}>
+                        <IonSelectOption value="masculino">Masculino</IonSelectOption>
+                        <IonSelectOption value="femenino">Femenino</IonSelectOption>
                     </IonSelect>
                 </IonItem>
                 <IonAlert
@@ -72,15 +72,15 @@ const Profile: React.FC = () => {
                     message={message}
                     buttons={['OK']}
                 />
-          </IonRow>
-          <IonRow class='space'></IonRow>
-          <IonRow className='align-center'>
-              <IonButton color='light-green' onClick={editUser}>Guardar cambios</IonButton>
-              <IonButton href="MainMenu"> Regresar  </IonButton>
-          </IonRow>
-      </IonContent>
+            </IonRow>
+            <IonRow class='space'></IonRow>
+            <IonRow className='align-center'>
+                <IonButton color='light-green' onClick={editUser}>Guardar cambios</IonButton>
+                <IonButton href="/MainMenu">Regresar</IonButton>
+            </IonRow>
+        </IonContent>
     );
-  };
+};
   
   export default Profile;
   
