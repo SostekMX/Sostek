16bad57dab5f96aaeb0d284cc264a5dc4910a062
LeNav23 <A01286259@tec.mx>
2026-06-05 01:43:05 -0600

    feat: migra presentaciones de drive a backend

--- src/pages/presentation/Presentation.tsx
+++ src/pages/presentation/Presentation.tsx
@@ -1,55 +1,59 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { IonContent, IonImg, IonLoading, IonPage } from '@ionic/react';
+import { IonContent, IonPage } from '@ionic/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Lazy } from 'swiper';
+import axios from 'axios';
 import AppBarPopOver from '../../components/layout/AppBarPopOver';
-import useGetPresentationImages from '../../hooks/useGetPresentationImages';
 import './Presentation.css';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/lazy';
 import '@ionic/react/css/ionic-swiper.css';
 
-  interface RouteParams{
-    driveId : string,
+interface RouteParams {
+  driveId: string;
 }
+
 const Presentation: React.FC = () => {
-  const {driveId} = useParams<RouteParams>();
-  const {urlImages, loading} = useGetPresentationImages(driveId);
-  const [imgLoading, setImgLoading] = useState(true);
-  // console.log(urlImages)
+  const { driveId } = useParams<RouteParams>();
+  const [slides, setSlides] = useState<string[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/presentations')
+      .then(res => {
+        if (res.data.success) {
+          const match = res.data.presentations.find((p: any) => p._id === driveId);
+          if (match) setSlides(match.slides);
+        }
+      })
+      .catch(err => console.log(err))
+      .finally(() => setLoading(false));
+  }, [driveId]);
+
   return (
     <IonPage>
-        <AppBarPopOver></AppBarPopOver>
-        <IonContent fullscreen class='bg-img'>
-        <>
-        {
-        !loading && <Swiper
-        className='mySwiper'
-        direction={"vertical"}
-        modules={[Pagination, Lazy]}
-        pagination={true}
-        lazy={true}
-        >
-          {
-            urlImages?.map((url : any) => {
-                // console.log(url.id);
-                return (
-                    <SwiperSlide>
-                      <IonImg 
-                    onIonImgDidLoad={() => {setImgLoading(false)}}
-                    onError={(e) => {console.log(e)}}
-                    src={ imgLoading ? "https://mir-s3-cdn-cf.behance.net/project_modules/disp/35771931234507.564a1d2403b3a.gif" : `https://drive.google.com/uc?id=${url.id}`}/></SwiperSlide>
-                )
-            })
-          }
-        </Swiper>
-        }
-        </>
+      <AppBarPopOver />
+      <IonContent fullscreen class='bg-img'>
+        {!loading && (
+          <Swiper
+            className='mySwiper'
+            direction={"vertical"}
+            modules={[Pagination, Lazy]}
+            pagination={true}
+            lazy={true}
+          >
+            {slides.map((url, index) => (
+              <SwiperSlide key={index}>
+                <img src={url} alt={`slide ${index + 1}`} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </IonContent>
-      </IonPage>
-  )
-}
+    </IonPage>
+  );
+};
 
 export default Presentation;
