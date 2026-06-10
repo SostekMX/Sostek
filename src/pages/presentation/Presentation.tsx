93bf1e9a60ef3eb4151becfffbcf4438b6ce52fa
LeNav23 <A01286259@tec.mx>
2026-06-10 17:38:41 -0600

    feat: agrega boton de regreso en presentaciones y arregla slide inicial

--- src/pages/presentation/Presentation.tsx
+++ src/pages/presentation/Presentation.tsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router';
-import { IonContent, IonPage } from '@ionic/react';
+import { useHistory } from 'react-router-dom';
+import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonButton, IonIcon, IonList, IonItem, IonLabel, IonPopover } from '@ionic/react';
+import { menuOutline, heart, personCircle, settings, logOut } from 'ionicons/icons';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Lazy } from 'swiper';
 import axios from 'axios';
 import { BACKEND_URL } from '../../config';
-import AppBarPopOver from '../../components/layout/AppBarPopOver';
 import './Presentation.css';
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -20,6 +21,10 @@ const Presentation: React.FC = () => {
   const { driveId } = useParams<RouteParams>();
   const [slides, setSlides] = useState<string[]>([]);
   const [loading, setLoading] = useState(true);
+  const { current: popoverId } = useRef(`pres-menu-${Math.random().toString(36).substr(2, 6)}`);
+  const history = useHistory();
+
+  const isUserLogged = sessionStorage.getItem('login') === 'true';
 
   useEffect(() => {
     axios.get(`${BACKEND_URL}/presentations`)
@@ -33,17 +38,45 @@ const Presentation: React.FC = () => {
       .finally(() => setLoading(false));
   }, [driveId]);
 
+  function logOutUser() {
+    sessionStorage.setItem('login', 'false');
+    sessionStorage.removeItem('token');
+    history.replace('/');
+  }
+
   return (
     <IonPage>
-      <AppBarPopOver />
+      <IonHeader>
+        <IonToolbar color="primary">
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/tab1" text="" />
+          </IonButtons>
+          <IonButtons slot="end">
+            <IonButton id={popoverId}>
+              <IonIcon slot="icon-only" icon={menuOutline} />
+            </IonButton>
+            <IonPopover trigger={popoverId} triggerAction="click">
+              <IonList>
+                {isUserLogged && <IonItem href="/Favorites"><IonIcon icon={heart} color="secondary" />&nbsp;<IonLabel>Favoritos</IonLabel></IonItem>}
+                {isUserLogged && <IonItem href="/Profile"><IonIcon icon={personCircle} color="secondary" />&nbsp;<IonLabel>Perfil</IonLabel></IonItem>}
+                {!isUserLogged && <IonItem href="/"><IonIcon icon={personCircle} color="secondary" />&nbsp;<IonLabel>Iniciar Sesión</IonLabel></IonItem>}
+                {isUserLogged && <IonItem><IonIcon icon={settings} color="secondary" />&nbsp;<IonLabel>Ajustes</IonLabel></IonItem>}
+                {isUserLogged && <IonItem onClick={logOutUser}><IonIcon icon={logOut} color="secondary" />&nbsp;<IonLabel>Log Out</IonLabel></IonItem>}
+              </IonList>
+            </IonPopover>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
       <IonContent fullscreen class='bg-img'>
         {!loading && (
           <Swiper
+            key={driveId}
             className='mySwiper'
             direction={"horizontal"}
             modules={[Pagination, Lazy]}
             pagination={true}
             lazy={true}
+            initialSlide={0}
           >
             {slides.map((url, index) => (
               <SwiperSlide key={index}>
